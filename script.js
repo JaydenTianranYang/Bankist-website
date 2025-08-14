b4d8dba78d303e8b8272d745ff05b3fbd9180820
Jayden Yang <jayden.yang@rea-group.com>
2025-08-14 20:15:44 +1000

    Implement dot logic

--- script.js
+++ script.js
@@ -234,6 +234,7 @@ imgTargets.forEach(img => {
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 
 let curSlide = 0;
 const maxSlide = slides.length;
@@ -244,12 +245,26 @@ const maxSlide = slides.length;
 
 // slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
 
+const createDots = function () {
+  slides.forEach((_, i) => {
+    dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`);
+  });
+};
+createDots();
+
+const activeDot = function (slide) {
+  document.querySelectorAll('.dots__dot').forEach(dot => {
+    dot.classList.remove('dots__dot--active');
+  });
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+}
+activeDot(0);
+
 const goToSlide = function (slide) {
   slides.forEach((s, i) => {
     s.style.transform = `translateX(${100 * (i - slide)}%)`;
   });
 };
-
 goToSlide(0);
 
 // Next slide
@@ -261,6 +276,7 @@ const nextSlide = function () {
   }
 
   goToSlide(curSlide);
+  activeDot(curSlide);
 }
 
 const prevSlide = function () {
@@ -271,11 +287,28 @@ const prevSlide = function () {
   }
 
   goToSlide(curSlide);
+  activeDot(curSlide);
 }
 
 btnRight.addEventListener('click', nextSlide);
 
 btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') prevSlide();
+  e.key === 'ArrowRight' && nextSlide();
+});
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    curSlide = Number(e.target.dataset.slide);
+    goToSlide(curSlide);
+    activeDot(curSlide);
+  }
+});
+
+
+
 
 
 /////////////////////////////////////////////////
